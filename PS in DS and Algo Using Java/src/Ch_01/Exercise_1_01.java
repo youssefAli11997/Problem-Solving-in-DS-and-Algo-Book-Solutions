6fcff4a31dd8dd1ec080a705cdf30e784e916055
youssefAli11997 <youssefali6212@gmail.com>
2017-08-05 23:45:32 +0200

    up

---
+++ PS in DS and Algo Using Java/src/Ch_01/Exercise_1_01.java
@@ -0,0 +1,16 @@
+package Ch_01;
+
+public class Exercise_1_01 {
+
+	public static int findAverage(int[] arr, int index, int sum){
+		if(index == arr.length)
+			return sum / arr.length;
+		return findAverage(arr, index + 1, sum + arr[index]);
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5};
+		System.out.println(findAverage(arr, 0, 0)); // should output 3
+	}
+
+}
