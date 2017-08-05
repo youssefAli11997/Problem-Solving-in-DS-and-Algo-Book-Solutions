92444b775491c53ea4553fb28177f088344ae336
youssefAli11997 <youssefali6212@gmail.com>
2017-08-06 00:45:33 +0200

    added 1_05 solution

---
+++ PS in DS and Algo Using Java/src/Ch_01/Exercise_1_05.java
@@ -0,0 +1,36 @@
+/*
+ * Find the second largest number in the array.
+ */
+
+package Ch_01;
+
+public class Exercise_1_05 {
+
+	public static int secondLargest(int[] arr){
+		int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] > max){
+				secondMax = max;
+				max = arr[i];
+			}
+			else if(arr[i] > secondMax)
+				secondMax = arr[i];
+		}
+		return secondMax;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {5,3,1,4,2};
+		System.out.println(secondLargest(arr)); // outputs 4
+		
+		int[] arr2 = {-1,3,-2,4,-5};
+		System.out.println(secondLargest(arr2)); // outputs 3
+		
+		int[] arr3 = {0,0,0,0,0};
+		System.out.println(secondLargest(arr3)); // outputs 0
+		
+		int[] arr4 = {11,3,-2,4,-5}; // outputs 4
+		System.out.println(secondLargest(arr4));
+	}
+
+}
