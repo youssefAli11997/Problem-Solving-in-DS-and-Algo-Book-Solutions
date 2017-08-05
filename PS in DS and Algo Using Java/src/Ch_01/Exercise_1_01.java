7da1a8ea0dbd145bba8a638765f9637b97d1e0d1
youssefAli11997 <youssefali6212@gmail.com>
2017-08-05 23:50:07 +0200

    added the iterative version

--- PS in DS and Algo Using Java/src/Ch_01/Exercise_1_01.java
+++ PS in DS and Algo Using Java/src/Ch_01/Exercise_1_01.java
@@ -1,16 +1,26 @@
 package Ch_01;
 
 public class Exercise_1_01 {
-
-	public static int findAverage(int[] arr, int index, int sum){
+	
+	// recursive
+	public static double findAverage(int[] arr, int index, int sum) {
 		if(index == arr.length)
-			return sum / arr.length;
+			return sum * 1.0 / arr.length;
 		return findAverage(arr, index + 1, sum + arr[index]);
+	}
+	
+	// iterative
+	public static double findAverage(int[] arr) {
+		int sum = 0;
+		for(int num : arr) {
+			sum += num;
+		}
+		return sum * 1.0 / arr.length;
 	}
 	
 	public static void main(String[] args) {
 		int[] arr = {2,4,5,1,3};
-		System.out.println(findAverage(arr, 0, 0)); // should output 3
+		System.out.println(findAverage(arr, 0, 0)); // should output 3.0
 	}
 
 }
