7d6b5ff7b4ecc4d10ef98e881b880ccd2f0615a3
youssefAli11997 <youssefali6212@gmail.com>
2017-08-06 01:40:06 +0200

    added another solution

--- PS in DS and Algo Using Java/src/Ch_01/Exercise_1_08.java
+++ PS in DS and Algo Using Java/src/Ch_01/Exercise_1_08.java
@@ -1,6 +1,8 @@
 /*
  * Given an array with value 0 or 1, write a program to segregate 0 on the left side and 1 on
  * the right side.
+ * 
+ * NOTE : this solution also works for problem 1_12
  */
 
 package Ch_01;
@@ -26,6 +28,33 @@ public class Exercise_1_08 {
 		}
 	}
 	
+	// another solution
+	public static void segregate2(int[] arr) {
+		for(int s = 0, e = arr.length - 1; s < e; ) {
+			if(arr[e] == 0 && arr[s] == 0)
+				s++;
+			else if(arr[e] == 0 && arr[s] == 1){
+				arr[e] = 1;
+				arr[s] = 0;
+				s++;
+				e--;
+			}
+			
+			else if(arr[s] == 1 && arr[e] == 1)
+				e--;
+			else if(arr[s] == 1 && arr[e] == 0){
+				arr[e] = 1;
+				arr[s] = 0;
+				s++;
+				e--;
+			}
+			else{
+				s++;
+				e--;
+			}
+		}
+	}
+	
 	public static void printArr(int[] arr) {
 		for(int num : arr){
 			System.out.print(num + " ");
@@ -38,6 +67,13 @@ public class Exercise_1_08 {
 		printArr(arr);
 		segregate(arr);
 		printArr(arr);
+		
+		System.out.println();
+		
+		int[] arr2 = {1,0,1,0,0,1,0,1,1,0};
+		printArr(arr2);
+		segregate2(arr2);
+		printArr(arr2);
 	}
 
 }
